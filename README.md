1c659d8e4e935c26dfca0bc09f1713080ba60fa9
Pavel Krayzel <pavel.krayzel@made.com>
2020-02-21 11:03:40 +0100

    Test

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # serverless-google-api
-serverless-google-api
+
+This repo is based on this article for serverless: https://serverless.com/blog/serverless-python-packaging/
+
+## Prerequisities
+
+- node
+- npm
+- python 3.6
+- AWS credentials configured
+
+## Local setup
+
+Create & activate virtual environment 
+
+```
+virtualenv google -p python3.6
+source google/bin/activate
+```
+
+Install dependencies
+
+``` 
+pip install -r hello-world/requirements.txt
+```
+
+## Deployment to AWS
+
+```
+serverless deploy
+```
---
+++ hello-world/handler.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+from google.cloud import texttospeech
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+from google.cloud import translate_v3
+
+
+def translate_text(text, target='en'):
+    """
+    Target must be an ISO 639-1 language code.
+    https://cloud.google.com/translate/docs/languages
+    """
+    client = translate_v3.TranslationServiceClient()
+    result = client.translate(
+        text,
+        target_language=target)
+
+    print(u'Text: {}'.format(result['input']))
+    print(u'Translation: {}'.format(result['translatedText']))
+    print(u'Detected source language: {}'.format(
+        result['detectedSourceLanguage']))
+
+    return result
+
+
+def hello(event, context):
+    credentials = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+    example_text = "Hola amigos"
+    translation = translate_text(example_text, target='en')
+
+    body = {
+        "source_message": example_text,
+        "message": translation,
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+    }
+
+    return response
+
+
+# running locally
+if __name__ == "__main__":
+    hello(None, None)
